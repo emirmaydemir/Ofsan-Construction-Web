e06ec7739fe15b0403401e48f792b5dfcc110e08
Emir Muhammet Aydemir <emirmaydemir@gmail.com>
2025-05-04 14:17:17 +0300

    up v4

--- contractor-app/app/(en)/en/projects/components/Project.tsx
+++ contractor-app/app/(en)/en/projects/components/Project.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { LabelWithIcon } from "./LabelWithIcon";
-import { StrapiImage } from "@/components/StrapiImage";
+import Image from "next/image";
 
 interface ProjectProps {
   title: string;
@@ -13,7 +13,7 @@ export const Project = ({ title, year, location, images }: ProjectProps) => {
   return (
     <div>
       <div className="relative h-56 w-full transform transition-transform hover:scale-105 hover:opacity-70">
-        <StrapiImage src={images[0]} alt="project image" fill className="object-cover" />
+        <Image src={images[0]} alt="project image" fill className="object-cover" />
         <div className="absolute bottom-1 right-1 gap-1">
           <div className="flex gap-1">
             <LabelWithIcon text={location} icon="/location.png" alt="location" />
